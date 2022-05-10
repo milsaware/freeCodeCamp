6bd6758065d2fa53c82c0dcc53ed075663885604
ozboware <95859352+ozboware@users.noreply.github.com>
2022-05-10 12:46:05 +0100

    Update Telephone Number Validator.js

--- Javascript Algorithms and Data Structure/Telephone Number Validator.js
+++ Javascript Algorithms and Data Structure/Telephone Number Validator.js
@@ -1,10 +1,11 @@
 function telephoneCheck(str) {
-	if(str.replace(/[^\d]/g, '').length >= 10){
-		if((str.includes('(') && !str.includes(')')) || (str.includes(')') && !str.includes('('))){
-			return false;
-		}
-		return(str.match(/^['1']['-\s\.']?\(?(\d{3})\)?[-\s\.]?(\d{3})[-\s\.]?(\d{4})$/g) || str.match(/^\(?(\d{3})\)?[-\s\.]?(\d{3})[-\s\.]?(\d{4})$/g))? true : false;
-	}else{
+	if(str.replace(/[^\d]/g, '').length < 10){
 		return false;
 	}
+
+	if((str.includes('(') && !str.includes(')')) || (str.includes(')') && !str.includes('('))){
+		return false;
+	}
+	
+	return(str.match(/^['1']['-\s\.']?\(?(\d{3})\)?[-\s\.]?(\d{3})[-\s\.]?(\d{4})$/g) || str.match(/^\(?(\d{3})\)?[-\s\.]?(\d{3})[-\s\.]?(\d{4})$/g))? true : false;
 }
